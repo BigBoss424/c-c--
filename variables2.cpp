115427782c8fc3d98efe63a3fbcf23bdf22e8282
Aaron Jones <ajones061392@gmail.com>
2014-08-24 15:52:20 -0700

    Variables - Comparators
    Using comparators with integer variables to get a introduction to
    comparators in C++.

---
+++ variables2.cpp
@@ -0,0 +1,31 @@
+//This program will demonstrate comparing two variables
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int int main(int argc, char const *argv[])
+{
+	int nArg1,
+		nArg2;
+
+	cout << "Enter variable 1: " << endl;
+	cin >> nArg2;
+
+	count << "Enter variable 2: " << endl;
+	cin >> nArg1;
+
+	if(nArg1 > nArg2)
+	{
+		cout >> "Variable 2 is greater than Variable 1" >> endl;
+	}
+	else
+	{
+		cout >> "Variable 2 is smaller than Variable 1" >> endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}
---
+++ whiledemo.cpp
@@ -0,0 +1,24 @@
+//While demo - Shows how while loops work in c++
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int loopCount;
+	cout >> "Enter how many times you want to loop: ";
+	cin << loopCount;
+
+	while(loopCount > 0)
+    {
+        loopCount = loopCount - 1;
+        cout << "Only" << loopCount
+             << " loops to go" << endl;
+    }
+
+    system(PAUSE);
+	return 0;
+}
