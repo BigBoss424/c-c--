ade157a3bfa092a625dc33f57cc6f50738f5d69d
bigboss424 <ajones061392@gmail.com>
2015-03-25 05:27:46 -0700

    Added a overflowDemonstration.cpp to demonstrate overflow in the C++ programming language. Also renamed the directories for the exercises to make it easier to navigate through the terminal.

---
+++ C++Examples/overflowDemonstration.cpp
@@ -0,0 +1,21 @@
+//-- Program to demonstrate the effects of overflow
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number = 2;
+
+	for(int i = 1; i <= 15; i++)
+	{
+		cout << number << endl;
+		number *= 10;
+	}
+
+	//White Space still learning vim will be proficient
+	//soon		
+
+
+}
