104afc5e131729baeff383b523c55bd4d05cb666
bigboss424 <ajones061392@gmail.com>
2020-01-13 22:10:57 -0800

    Added a new folder for the personal finance programs I am currently building. Toward a GUI application that will better manage my finances.

---
+++ c++_Finances/IntRateCalculator/IntRateCalculator.cpp
@@ -0,0 +1,41 @@
+//
+//  IntRateCalculator.cpp
+//  IntRateCalculator class file
+
+#include "IntRateCalculator.h"
+
+IntRateCalculator::IntRateCalculator(double rate)
+: m_rate(rate)
+{
+
+}
+
+IntRateCalculator::~IntRateCalculator(){
+
+}
+
+//IntRateCalculator::IntRateCalculator(const IntRateCalculator &v)
+//{
+
+//}
+
+//IntRateCalculator::~IntRateCalculator()
+//{
+//  }
+
+IntRateCalculator::IntRateCalculator(const IntRateCalculator &v)
+    :m_rate(v.m_rate)
+{
+
+}
+
+IntRateCalculator &IntRateCalculator::operator=(const IntRateCalculator &v)
+{
+    if (&v != this)
+    {
+        this->m_rate = v.m_rate;
+    }
+    return *this;
+}
+
+//
