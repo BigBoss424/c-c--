5d10da7a367b8c508295b66f988f3c5422ac8ecb
bigboss424 <ajones061392@gmail.com>
2020-01-25 22:47:28 -0800

    Added the header file for the compound int rate calculator program.

---
+++ c++_Finances/compoundInterest/CompoundIntRateCalculator.h
@@ -0,0 +1,10 @@
+// CompoundIntRateCalculator.h
+// CompoundIntRateCalculator header file
+
+#ifndef __FinancialSamples__CompoundIntRateCalculator__
+#define __FinancialSamples__CompoundIntRateCalculator__
+
+class CompoundIntRateCalculator {
+    public:
+
+#endif
