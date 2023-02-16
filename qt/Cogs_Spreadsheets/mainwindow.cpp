003eb8a2bf55a7b950d279a890fb3bcaf874b77d
ajones424 <ajones061392@yahoo.com>
2023-02-15 19:41:45 -0800

    Added some new examples and source code from the QT framework. Learning how to create GUI applications.

--- qt/Cogs_Spreadsheets/mainwindow.cpp
+++ qt/Cogs_Spreadsheets/mainwindow.cpp
@@ -1,15 +1,59 @@
+#include <QtGui>
+
+#include "finddialog.h"
+#include "gotocelldialog.h"
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+//#include "ui_mainwindow.h"
+#include "sortdialog.h"
+#include "spreadsheet.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
 
+MainWindow::MainWindow(){
+    spreadsheet = new Spreadsheet();
+    setCentralWidget(spreadsheet);
+    
+    createActions();
+    createMenus();
+    createToolBars();
+    createStatusBar();
+    createContextMenu();
+    
+    readingSettings();
+    
+    findDialog = 0;
+    
+    setWindowIcon(QIcon(":/images/icon"));
+    setCurrentFile("");
+}//endofMainWindow
+
+void MainWindow::createActions(){
+    newAction = new QAction(tr("&New"), this);
+    newAction ->setIcon((":/images/new.png"));
+    newAction ->setShortcut(QKeySequence::New);
+    newAction ->setStatusTip(tr("Create a new spreadsheet file"));
+    connect(newAction, SIGNAL(triggered()), this, SLOT(newFile()));
+    
+    for(int i = 0; i < MaxRecentFiles; ++)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}//end of createActions()
--- qt/Cogs_Spreadsheets/mainwindow.h
+++ qt/Cogs_Spreadsheets/mainwindow.h
@@ -3,6 +3,12 @@
 
 #include <QMainWindow>
 
+class QAction;
+class QLabel;
+class FindDialog;
+class Spreadsheet;
+
+    
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +21,60 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void goToCell();
+    void newFile();
+    void open();
+    void find();
+    void sort();
+    void about();
+    bool save();
+    bool saveAs();
+
+    void openRecentFile();
+    void updateStatusBar();
+    void spreadsheetModified();
+
 private:
-    Ui::MainWindow *ui;
+    void createActions();
+    void createMenus();
+    void createContextMenu();
+    void createToolBars();
+    void createStatusBar();
+    void readSettings();
+    void writeSettings();
+    void updateRecentFileActions();
+    void setCurrentFile(const QString &filename);
+    bool okToContinue();
+    bool saveFile(const QString &filename);
+    bool loadFile(const QString &filename);
+    QString strippedName(const QString &fullFileName);
+
+    Spreadsheet *spreadsheet;
+    FindDialog findDialog;
+    QLabel locationLabel;
+    QLabel formulaLabel;
+    QStringList recentFiles;
+    QString curFile;
+
+    enum { MaxRecentFiles = 5};
+    QAction seperatorAction;
+    QAction recentFileActions[MaxRecentFiles];
+
+    QMenu fileMenu;
+    QMenu editMenu;
+
+    QToolBar editToolBar;
+    QToolBar fileToolBar;
+    QAction openAction;
+    QAction newAction;
+
+    QAction aboutQtAction;
+
+//private:
+//    Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
